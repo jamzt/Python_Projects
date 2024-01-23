8968dd49efb47b6794755db11a5a15a53e4ae99f
jamzt <jamietaalai@gmail.com>
2024-01-23 14:38:03 -0800

    webpagegen assignment

--- webpagegen.py
+++ webpagegen.py
@@ -7,11 +7,15 @@ class ParentWindow(Frame):
         Frame.__init__(self, master)
         self.master.title("Web Page Generator")
     
-        self.btn = Button(self.master, text="Default HTML Page", width=30, height=2, command=self.defaultHTML)
+        self.btn = Button(text="Default HTML Page", width=30, height=2, command=self.defaultHTML)
         self.btn.grid(padx=(10,10), pady=(10,10))
 
-        self.btn_cstm = Button(self.master, text="Custom HTML Page", width=30, height=2, command=self.customHTML)
-        self.btn_cstm.grid(padx=(10, 10), pady=(10, 10))
+        #entry widget
+        self.cstm_entry = Entry(width=40)
+        self.cstm_entry.grid(row=1, column=0, columnspan=2, padx=(10,10), pady=(0, 10)) 
+
+        self.btn_cstm = Button(text="Custom HTML Page", width=30, height=2, command=self.customHTML)
+        self.btn_cstm.grid(row=2, column=0, columnspan=2, padx=(10, 10), pady=(10, 10))
 
     def defaultHTML(self):
         htmlText = "Stay tuned for our amazing summer sale!"
@@ -22,9 +26,9 @@ class ParentWindow(Frame):
         webbrowser.open_new_tab("index.html")
 
     def customHTML(self):
-        user_input = input("Enter custom text and press Submit Custom Text")
+        user_input = self.cstm_entry.get()
         htmlFile = open("index.html", "w")
-        htmlContent = "<html>\n<body>\n<h1>" + {user_input} + "</h1>\n</body>\n</html>"
+        htmlContent = "<html>\n<body>\n<h1>" + user_input + "</h1>\n</body>\n</html>"
         htmlFile.write(htmlContent)
         htmlFile.close()
         webbrowser.open_new_tab("index.html")
@@ -34,4 +38,5 @@ if __name__ == "__main__":
     App = ParentWindow(root)
     root.mainloop()
 
-#input field in the GUI 
+#input field in the GUI 45
+    
